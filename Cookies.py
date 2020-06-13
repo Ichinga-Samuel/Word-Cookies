58121fad9c2d0b19671486d4df85f0bc6744eb1a
Ichinga Samuel <46368656+Aka-Ikenga@users.noreply.github.com>
2020-06-13 13:37:04 +0100

    Add files via upload

---
+++ Cookies.py
@@ -0,0 +1,20 @@
+import itertools as it
+import pprint as pp
+
+
+def word_cookies(cookie):
+	with open('Common English Words.txt', 'r') as fh:
+		fh = fh.readlines()
+		valid_words = {i.strip() for i in fh}  # a set containing all the words
+		cookies = {}
+		for i in range(3, len(cookie)+1):
+			# permutation produces invalid words, an intersection with the set of valid words will return
+			# the valid words produced by the permutation operation
+			words = {''.join(i) for i in it.permutations(cookie, i)} & valid_words
+			if words != set():
+				cookies[f'{i} Letter Words'] = words
+
+		pp.pprint(cookies, indent=2)
+
+
+word_cookies('Excellent')
